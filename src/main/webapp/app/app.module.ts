7616293cf8dc70b37f5fbac934921c1d58cb9ec1
Son Pham <spham1412@gmail.com>
2017-06-04 18:52:33 -0500

    Initial CMS Setup

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,52 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { CmsSharedModule, UserRouteAccessService } from './shared';
+import { CmsHomeModule } from './home/home.module';
+import { CmsAdminModule } from './admin/admin.module';
+import { CmsAccountModule } from './account/account.module';
+import { CmsEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        CmsSharedModule,
+        CmsHomeModule,
+        CmsAdminModule,
+        CmsAccountModule,
+        CmsEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class CmsAppModule {}
